7ba99c12d8543705de87b5a00a679d76d4aef8de
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-28 17:41:17 -0500

    rendered pizzas

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Pizza from "./Pizza";
+import pizzas from "../data/pizzas.json";
 
 const App = () => {
-	return <h1>Hello World!</h1>;
+	return (
+		<ul>
+			{pizzas.map((pizza) => {
+				return <Pizza key={pizza.id} pizza={pizza} />;
+			})}
+		</ul>
+	);
 };
 
 export default App;

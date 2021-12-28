eccbdab85be76352b47ba1bf9923ae320e3d149f
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-28 17:38:43 -0500

    deleted not needed changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/App";
-import pizzas from "./data/pizzas.json";
 
-alert("hello World!");
 ReactDom.render(<App />, document.getElementById("root"));

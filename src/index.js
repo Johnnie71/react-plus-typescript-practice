7ae17b2daf16603c1fe57991e2a354a8f37881a5
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-28 14:51:44 -0500

    installed babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert("Hello World!");
+import React from "react";
+import ReactDom from "react-dom";
+
+const HelloWorld = () => {
+	return <h1>Hello World!</h1>;
+};
+
+ReactDom.render(<HelloWorld />, document.getElementById("root"));
